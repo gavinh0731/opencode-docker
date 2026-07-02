8127446647b07e5f93ea501d776987eeea4580a7
gavin <gavin@hisharp.com.tw>
2026-07-02 02:05:55 +0000

    opencode 可以透過 build.sh 重新編譯新版 Image

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
+# 建立 OpenCode AI Docker Base 映像檔
+sudo docker build -f Dockerfile.base -t opencode-base .;
 # 建立 OpenCode AI Docker 映像檔
-docker build -t opencode-ai:latest .
+sudo docker build -f Dockerfile -t opencode-ai:latest . --no-cache;
